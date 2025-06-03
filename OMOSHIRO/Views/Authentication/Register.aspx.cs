f45ce6c6d5ceaa2a22af3258692c57e2e0e3a7af
harraz masri <harraz@rekasawang.com>
2025-06-03 11:24:52 +0800

    register user

--- OMOSHIRO/Views/Authentication/Register.aspx.cs
+++ OMOSHIRO/Views/Authentication/Register.aspx.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,5 +19,59 @@ namespace OMOSHIRO.Views.Authentication
 		{
 
 		}
-	}
+
+        protected void RegisterButton_Click(object sender, EventArgs e)
+        {
+            string HashPassword(string password)
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(password);
+                    byte[] hash = sha256.ComputeHash(bytes);
+                    StringBuilder builder = new StringBuilder();
+                    foreach (var b in hash)
+                        builder.Append(b.ToString("x2")); // convert to hex string
+                    return builder.ToString();
+                }
+            }
+
+            try
+            {
+                if (Password.Text == RepPassword.Text)
+                {
+                    string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                    using (SqlConnection conn = new SqlConnection(connStr))
+                    {
+                        string query = @"
+                            INSERT INTO Users (username, full_name, email, role, password)
+                            VALUES (@username, @full_name, @email, @role, @password);";
+
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@username", UserName.Text);
+                            cmd.Parameters.AddWithValue("@full_name", FullName.Text);
+                            cmd.Parameters.AddWithValue("@email", Email.Text);
+                            cmd.Parameters.AddWithValue("@role", "users");
+
+                            string hashedPassword = HashPassword(Password.Text);
+                            cmd.Parameters.AddWithValue("@password", hashedPassword);
+
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    Response.Redirect("~/Views/Authentication/Login.aspx");
+                } else
+                {
+                    lblMessage.Text = "Password doesn't match.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = "Error: " + ex.Message;
+                return;
+            }
+        }
+    }
 }
