ba5629a5f0485f86b47e43ef02babd052a3dd0ea
harraz masri <harraz@rekasawang.com>
2025-06-05 00:12:59 +0800

    About Business

--- OMOSHIRO/Views/Dashboard.Master.designer.cs
+++ OMOSHIRO/Views/Dashboard.Master.designer.cs
@@ -51,6 +51,15 @@ namespace OMOSHIRO.Views
         protected global::System.Web.UI.WebControls.ContentPlaceHolder DashboardPlaceholder;
 
         /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+
+        /// <summary>
         /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
